6bdd48d00351a77a97f0900d010e8766d7fbbda0
Gyanendra Priyadarshi <157kuchi@gmail.com>
2020-04-27 11:33:31 +0530

    Bug fixing

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ require("./config/passport")(passport);
 const db = require("./config/keys").mongoURI;
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
