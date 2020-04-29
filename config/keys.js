6168fb81028c181b00f156896c86957b286d0c44
Gyanendra Priyadarshi <157kuchi@gmail.com>
2020-04-29 20:42:30 +0530

    chang in mongodb database

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = "mongodb://127.0.0.1:27017/event-manager"; //Use your Mongodb Atlas key
+dbPassword = "mongodb://127.0.0.1:27017/event-manager"; //Use your Mongodb Atlas key for online purpose
 
 module.exports = {
   mongoURI: dbPassword,
